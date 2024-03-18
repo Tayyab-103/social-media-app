7d5e0775b0d7bc4d1ce30f006dab3f470c4b2f34
Tayyab-103 <tayyabhameed103@gmail.com>
2024-03-18 16:27:05 +0500

    Social Media App basic UI Done

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,18 @@
-import Post from "./Post"
+import { useContext } from "react";
+import Post from "./Post";
+import { PostList as PostListData } from "../store/post-list-store";
 
 const PostList = () => {
+  const { postList } = useContext(PostListData);
+  console.log(postList,"Post Update")
   return (
     <>
-    <Post/>
-    <Post/>
-    <Post/>
-    </>
-  )
-}
+      {postList.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
 
-export default PostList
+    </>
+  );
+};
+
+export default PostList;
--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -1,15 +1,50 @@
 import { createContext, useReducer } from "react";
 
-const PostList = createContext({
+export const PostList = createContext({
   postList: [],
   addPost: () => {},
-  delete: () => {},
+  deletePost: () => {
+   
+  },
 });
+
+const postListReducer = (currentPostList, action) => {
+  return currentPostList;
+};
 
 const PostListProvider = ({ children }) => {
   // const [state, diapatch] = useReducer(reducerFunction, defaultValue);
-  const [postList, dispatchPostList] = useReducer();
-  return <PostList.Provider value={[]}>{children}</PostList.Provider>;
+  // const [postList, dispatchPostList] = useReducer(postListReducer, DEFAULT_CONTEXT);
+  const [postList, dispatchPostList] = useReducer(postListReducer, DEFAULT_POST_LIST);
+
+  const addPost = () => {};
+  const deletePost = (postId) => {
+    console.log(`Delete Post Called for ${postId}`)
+  };
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
 };
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to Pakistan",
+    body: "Hi Friends, I am going to Pakistan for my vacations, Hope to Enjoy a lot. Peace out",
+    reactions: 2,
+    userId: "user-9",
+    tags: ["vacation", "Pakistan", "Enjoying"],
+  },
+  {
+    id: "2",
+    title: "Pass ho gaye bhai",
+    body: "char saal ki masti ky baad bi ho gaye hy pass",
+    reactions: 100,
+    userId: "user-12",
+    tags: ["Graduating", "Unbelievable"],
+  },
+];
 
 export default PostListProvider;
